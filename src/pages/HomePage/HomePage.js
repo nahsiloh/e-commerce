a680d5b00872d4f58a4774a3e0977ff5f211eba0
lishan_ho <lishan.ho@emporioanalytics.com>
2020-07-22 19:42:13 +0800

    Created homepage and menu items

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.styles.scss";
+import Directory from "../../components/Directory/Directory";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+export default HomePage;
