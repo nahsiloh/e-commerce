e6fef2e85d68cdc032ce6bb9494eb0f82681b9bd
lishan_ho <lishan.ho@emporioanalytics.com>
2020-07-24 17:25:16 +0800

    added react router to menu-items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./HomePage.styles.scss";
 import Directory from "../../components/Directory/Directory";
 
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <div className="homepage">
       <Directory />
